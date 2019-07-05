e21490efb66e2a9d357c3d084fecd2b188514bba
Izzy Miller <izzymiller@iz-3.local>
2019-07-05 23:58:49 +0900

    add actual test

--- src/actions/absolve/test_absolve.ts
+++ src/actions/absolve/test_absolve.ts
@@ -1,84 +1,64 @@
 import * as chai from "chai"
-import * as sinon from "sinon"
 
 import * as Hub from "../../hub"
 
-import { DigitalOceanDropletAction } from "./digitalocean_droplet"
+import { absolveAction } from "./absolve"
 
-const action = new DigitalOceanDropletAction()
-
-function expectDigitalOceanDropletActionMatch(request: Hub.ActionRequest, ...match: any[]) {
-
-  const dropletsRequestActionSpy = sinon.spy(
-    (dropletId: string, params: any, callback: (err: any, data: any) => void) => {
-    callback(null, `successfully called with ${dropletId} ${params}`)
-  })
-  const stubClient = sinon.stub(action as any, "digitalOceanClientFromRequest")
-    .callsFake(() => ({
-      dropletsRequestAction: dropletsRequestActionSpy,
-    }))
-  return chai.expect(action.execute(request)).to.be.fulfilled.then(() => {
-    chai.expect(dropletsRequestActionSpy.firstCall).to.have.been.calledWithMatch(...match)
-    stubClient.restore()
-  })
-}
+const action = new absolveAction()
 
 describe(`${action.constructor.name} unit tests`, () => {
 
-  describe("action", () => {
-
-    it("errors if there is no attachment for query", () => {
-      const request = new Hub.ActionRequest()
-      request.type = Hub.ActionType.Query
-      request.params = {
-        digitalocean_api_key: "mykey",
-      }
-
-      return chai.expect(action.execute(request)).to.eventually
-        .be.rejectedWith("Couldn't get data from attachment.")
-    })
-
-    it("sends right params for query", () => {
-      const request = new Hub.ActionRequest()
-      request.type = Hub.ActionType.Query
-      request.params = {
-        digitalocean_api_key: "mykey",
-      }
-      request.attachment = {dataJSON: {
-        fields: [{name: "coolfield", tags: ["digitalocean_droplet_id"]}],
-        data: [
-          {coolfield: {value: "12345"}},
-        ],
-      }}
-      return expectDigitalOceanDropletActionMatch(request, 12345, {type: "power_off"})
-    })
-
-    it("errors if there is no attachment for cell", () => {
-      const request = new Hub.ActionRequest()
-      request.type = Hub.ActionType.Cell
-      request.params = {
-        digitalocean_api_key: "mykey",
-      }
-      return chai.expect(action.execute(request)).to.eventually
-        .be.rejectedWith("Couldn't get data from attachment.")
-    })
-
-    it("sends right params for cell", () => {
-      const request = new Hub.ActionRequest()
-      request.type = Hub.ActionType.Cell
-      request.params = {
-        digitalocean_api_key: "mykey",
-        value: "12345",
-      }
-      return expectDigitalOceanDropletActionMatch(request, 12345, {type: "power_off"})
-    })
-
-  })
-
   describe("form", () => {
 
-    it("doesn't have form", () => {
-      chai.expect(action.hasForm).equals(false)
+    it("has form", () => {
+      chai.expect(action.hasForm).equals(true)
+    })
+
+    it("has form with url param", (done) => {
+      const request = new Hub.ActionRequest()
+      const form = action.validateAndFetchForm(request)
+      chai.expect(form).to.eventually.deep.equal({
+        fields: [{
+          label: "Use Thresholds?",
+          name: "useThreshold",
+          required: true,
+          type: "select",
+          options: [
+            {name: "no", label: "No"},
+            {name: "yes", label: "Yes"},
+          ],
+          default: "yes",
+        },
+        {
+          label: "Percentage Margin Threshold",
+          name: "percentThreshold",
+          required: false,
+          type: "string",
+          default: "2"
+        },
+        {
+         label: "Manual Cost Threshold ($)",
+          name: "costThreshold",
+          required: false,
+          type: "string",
+          default: "",
+        },
+        {
+          label: "Offset Type",
+           description: "Type of REC. Recommended left blank for optimal price matching.",
+           name: "offsetType",
+           required: false,
+           type: "select",
+           options: [
+            {name: "wind", label: "Wind"},
+            {name: "solar", label: "Solar"},
+            {name: "biomass", label: "Biomass"},
+            {name: "solar", label: "Solar"}
+          ],
+           default: "",
+         },
+        ],
+      }).and.notify(done)
     })
 
   })
